4d2d8f66e07525f734c17e2d2b42feb6ac15b30d
Rebecca DuPrey <rduprey@asu.edu>
2025-11-03 13:33:42 -0500

    Add initial TicTacToe class with main method (amended for demo)

---
+++ src/TicTacToe.java
@@ -0,0 +1,8 @@
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("Welcome to TicTacToe!");
+        // TODO: Implement game logic
+    }
+}
+// temporary staging demo
+// temporary unstaged demo
